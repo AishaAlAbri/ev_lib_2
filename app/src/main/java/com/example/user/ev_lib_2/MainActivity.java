f77d5f16739f81c87c7f603d01f6f2db4879811c
aishaAlAbri <awaash-16.92@hotmail.com>
2016-03-17 10:00:23 +0400

    fragment

--- app/src/main/java/com/example/user/ev_lib_2/MainActivity.java
+++ app/src/main/java/com/example/user/ev_lib_2/MainActivity.java
@@ -92,17 +92,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.sign_in) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.my_books) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.contact_us) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.about_us) {
 
         }
 
