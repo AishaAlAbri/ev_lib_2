85c7852404156d07bb9776dedbdaf79db45d59b5
omaimaAlqasmi <omaima.alqasmi@gmail.com>
2016-03-23 15:08:01 +0400

    Editing design

--- app/src/main/java/com/example/user/ev_lib_2/Adult.java
+++ app/src/main/java/com/example/user/ev_lib_2/Adult.java
@@ -3,11 +3,11 @@ package com.example.user.ev_lib_2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Kid extends AppCompatActivity {
+public class Adult extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_kid);
+        setContentView(R.layout.activity_adult);
     }
 }
